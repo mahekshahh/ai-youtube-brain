7628ba671032a6a518c5f7cbdf17caeb82056c76
mahekshahh <mahekshah917@gmail.com>
2026-06-09 19:41:32 +0530

    Phase 1: transcript extractor done

---
+++ transcript.py
@@ -0,0 +1,15 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def get_video_id(url):
+    if "v=" in url:
+        return url.split("v=")[1].split("&")[0]
+    return url
+
+def get_transcript(url):
+    video_id = get_video_id(url)
+    transcript = YouTubeTranscriptApi().fetch(video_id)
+    full_text = " ".join([item.text for item in transcript])
+    return full_text
+
+url = input("Paste YouTube URL: ")
+print(get_transcript(url))
